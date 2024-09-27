a06401de8cf019b2a52f21e6e12606e257c082cb
gperez314 <g.perez314@mybvc.ca>
2024-09-27 00:34:03 -0600

    Basic functionality added

---
+++ Game.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SODV2101Rapid_Assignment01
+{
+    internal class Game
+    {
+        private static Player _player1 { get; set; }
+        private static Player _player2 { get; set; }
+        private static Board _board { get; set; }
+
+        public Game()
+        {
+            _board = new Board();
+        }
+
+        public int Turn(int cell)
+        {
+            return _board.Turn(cell);   
+        }
+
+        public bool CheckWin()
+        {
+            return _board.CheckWin();
+        }
+
+
+    }
+
+
+}
---
+++ Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SODV2101Rapid_Assignment01
+{
+    internal class Player
+    {
+        // Define properties for Player ID
+        public int ID { get; set; }
+
+        // Construtor for the Player class
+        public Player(int id)
+        {
+            ID = id;
+        }
+
+    }
+}
